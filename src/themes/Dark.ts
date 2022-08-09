7055e5285f35060a7200dd0e3946d8a942766f96
Aldcejam <aldcejamj923@gmail.com>
2022-08-08 23:23:50 -0300

    colores

--- src/themes/Dark.ts
+++ src/themes/Dark.ts
@@ -10,7 +10,7 @@ export const DarkTheme = (name: any) => {
       },
       action: {
         active: "#ffffff",
-        disabled: "#000",
+        disabled: "#000000",
       },
       background: {
         paper: "#353535",
