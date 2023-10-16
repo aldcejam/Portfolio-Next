b1359b722db33f8d1efe172215765daa729b57ee
aldcejam <aldcejamj923@gmail.com>
2023-10-16 12:50:09 -0300

    a

--- src/partils/header/Header.tsx
+++ src/partils/header/Header.tsx
@@ -1,17 +1,19 @@
 import { useState } from "react";
+import { Link } from "next/link";
 import MenuBurguer from "../../components/MenuBurguer/MenuBurguer";
 import { StyledHeader, StyledLog } from "./Styled.Header";
 
 const Header = () => {
 
     return (
-
-        <StyledHeader>
-            <StyledLog>
-                W
-            </StyledLog>
-            <MenuBurguer  />
-        </StyledHeader>
+        <Link href="/"> 
+            <StyledHeader>
+                <StyledLog>
+                    W
+                </StyledLog>
+                <MenuBurguer />
+            </StyledHeader>
+        </Link>
     )
 }
 
